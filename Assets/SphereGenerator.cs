6fda466c228e2a48b288a583f0f830c1f8830c66
Jun Shimura <2120254+JunShimura@users.noreply.github.com>
2025-03-07 20:36:10 +0900

    start deleted

--- Assets/SphereGenerator.cs
+++ Assets/SphereGenerator.cs
@@ -7,11 +7,6 @@ public class SphereGenerator : MonoBehaviour
     public float r;
     public float rate=0;
     float t=0.0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
